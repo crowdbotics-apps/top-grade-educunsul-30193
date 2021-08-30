a37bd9c5893d0ce1cc0cdd7a3bbca7a09556d1cb
Crowdbotics <team@crowdbotics.com>
2021-08-30 08:22:49 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "top_grade_educunsul_30193Identifier",
+  url: "https://top_grade_educunsul_30193Identifier.botics.co",
+  api: "https://top_grade_educunsul_30193Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
